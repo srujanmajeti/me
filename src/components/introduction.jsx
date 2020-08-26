ec1a990eb58d758855ea4b5fb36dca1cdbdb0f42
srujan <srujankumar.majeti@gmail.com>
2020-08-26 15:45:32 -0400

    Updating Changes

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -15,7 +15,7 @@ export default class Introduction extends Component {
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1>Hi! <br />I'm Srujan</h1>
-                          <p><a className="btn btn-primary btn-learn" href="Srujan_Resume.pdf" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                          <p><a className="btn btn-primary btn-learn" href="SaiSrujanKumar_Majeti.pdf" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
                         </div>
                       </div>
                     </div>
